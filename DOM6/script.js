90620485c83b99660a3ffeb65e6f6247f9b522b1
Minskoleopc <minskoleopc@gmail.com>
2022-12-21 19:03:19 +0530

    addded

--- DOM6/script.js
+++ DOM6/script.js
@@ -1,7 +1,6 @@
 let buttonA = document.querySelector('button')
 let liElements = document.querySelectorAll('li')
 console.log(liElements)
-
 // node list very similar to array
 // for(let i = 0 ; i < liElements.length ; i++){
 //     console.log(liElements[i].textContent)
